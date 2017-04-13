38731b4034ecdb77c5c382ac8fe686ff4671ff29
SaverioSun <Saverio.sun@gmail.com>
2017-04-13 15:51:15 +0800

    本次改动： 将网络该为5层网络（3层卷积网络+2层全连接网络） 测试集上正确率为77.35%

--- network_model.py
+++ network_model.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 
-def inference(images,batch_size,n_class):
+def inference(images,batch_size,n_class,keep_drop):
 
     with tf.variable_scope('conv1') as scope :
         weights=tf.get_variable('weights',
-                                [3,3,1,16],
+                                [3,3,1,32],
                                 dtype=tf.float32,
                                 initializer=tf.truncated_normal_initializer(stddev=0.1,dtype=tf.float32))
         bias=tf.get_variable('bias',
-                             [16],
+                             [32],
                              dtype=tf.float32,
                              initializer=tf.constant_initializer(0.1))
         conv=tf.nn.conv2d(images,weights,strides=[1,1,1,1],padding='SAME')
@@ -20,11 +20,11 @@ def inference(images,batch_size,n_class):
 
     with tf.variable_scope('conv2') as scope:
         weights=tf.get_variable('weights',
-                                [3,3,16,32],
+                                [3,3,32,64],
                                 dtype=tf.float32,
                                 initializer=tf.truncated_normal_initializer(stddev=0.1,dtype=tf.float32))
         bias=tf.get_variable('bias',
-                             [32],
+                             [64],
                              dtype=tf.float32,
                              initializer=tf.constant_initializer(0.1))
         conv=tf.nn.conv2d(pool1,weights,strides=[1,1,1,1],padding='SAME')
@@ -34,8 +34,24 @@ def inference(images,batch_size,n_class):
         pool2=tf.nn.max_pool(conv2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
 
 
-    with tf.variable_scope('fc3') as scope:
-        reshape=tf.reshape(pool2,[batch_size,-1])
+    with tf.variable_scope('conv3') as scope:
+        weights=tf.get_variable('weights',
+                                [3,3,64,128],
+                                dtype=tf.float32,
+                                initializer=tf.truncated_normal_initializer(stddev=0.1,dtype=tf.float32))
+        bias=tf.get_variable('bias',
+                             [128],
+                             dtype=tf.float32,
+                             initializer=tf.constant_initializer(0.1))
+        conv=tf.nn.conv2d(pool2,weights,strides=[1,1,1,1],padding='SAME')
+        conv3=tf.nn.relu(tf.nn.bias_add(conv,bias))
+
+    with tf.variable_scope('pool3') as scope:
+        pool3=tf.nn.max_pool(conv3,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+
+
+    with tf.variable_scope('fc4') as scope:
+        reshape=tf.reshape(pool3,[batch_size,-1])
         in_num=reshape.get_shape()[1].value
         weights=tf.get_variable('weights',
                                 [in_num,1024],
@@ -45,9 +61,9 @@ def inference(images,batch_size,n_class):
                              [1024],
                              dtype=tf.float32,
                              initializer=tf.constant_initializer(0.1))
-        fc3=tf.nn.relu(tf.nn.bias_add(tf.matmul(reshape,weights),bias))
+        fc4=tf.nn.relu(tf.nn.bias_add(tf.matmul(reshape,weights),bias))
 
-    with tf.variable_scope('fc4') as scope :
+    with tf.variable_scope('fc5') as scope :
         weights=tf.get_variable('weights',
                                 [1024,n_class],
                                 dtype=tf.float32,
@@ -56,9 +72,10 @@ def inference(images,batch_size,n_class):
                              [n_class],
                              dtype=tf.float32,
                              initializer=tf.constant_initializer(0.1))
-        fc4=tf.nn.bias_add(tf.matmul(fc3,weights),bias)
+        fc4=tf.nn.dropout(fc4,keep_prob=keep_drop)
+        fc5=tf.nn.bias_add(tf.matmul(fc4,weights),bias)
 
-    return fc4
+    return fc5
 
 def losses(logits,labels):
     with tf.name_scope('loss_layer'):
--- trainning.py
+++ trainning.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import tensorflow as tf
 import input_data
 import network_model
@@ -10,16 +11,18 @@ import matplotlib.pyplot as plt
 N_CLASS=3755
 IMG_W=64
 IMG_H=64
-BATCH_SIZE=80
+BATCH_SIZE=60
 CAPACTIY=2000
-MAX_STEP=50000
+MAX_STEP=160000
 learning_rate=0.0001
+isrestore=True
 log_dir='./log/'
 def run_training():
     train_dir='/home/saverio_sun/project/chinese_rec_data/train'
+    keep_prob=0.6
     images,labels=input_data.read_files(train_dir)
     train_batch,train_label=input_data.get_batch(images,labels,IMG_W,IMG_H,BATCH_SIZE,CAPACTIY)
-    train_logits=network_model.inference(train_batch,BATCH_SIZE,N_CLASS)
+    train_logits=network_model.inference(train_batch,BATCH_SIZE,N_CLASS,keep_prob)
     train_loss=network_model.losses(train_logits,train_label)
     train_op=network_model.training(train_loss,learning_rate)
     train_acc=network_model.evaluation(logits=train_logits,labels=train_label)
@@ -32,6 +35,14 @@ def run_training():
     sess.run(init)
     coord=tf.train.Coordinator()
     threads=tf.train.start_queue_runners(sess=sess,coord=coord)
+    start_step=0
+    if isrestore:
+        ckpt=tf.train.latest_checkpoint(log_dir)
+        if ckpt:
+            saver.restore(sess,ckpt)
+            print 'restore from the checkpoint {0}'.format(ckpt)
+            start_step+=int(ckpt.split('-')[-1])
+    print '::: Training Start:::'
     try:
         for step in range(MAX_STEP):
             if coord.should_stop():
@@ -41,7 +52,7 @@ def run_training():
                 print 'Step %d,train loss=%.2f, train accuracy=%.2f'%(step,tra_loss,tra_acc)
                 summary_str=sess.run(summary_op)
                 train_writer.add_summary(summary_str,global_step=step)
-            if step%2000==0 or (step+1)==MAX_STEP:
+            if step%10000==0 or (step+1)==MAX_STEP:
                 checkpoint_path=os.path.join(log_dir,'model.ckpt')
                 saver.save(sess,save_path=checkpoint_path,global_step=step)
     except tf.errors.OutOfRangeError:
@@ -51,7 +62,58 @@ def run_training():
     coord.join(threads)
     sess.close()
 
+def test():
+    test_dir='/home/saverio_sun/project/chinese_rec_data/test'
+    keep_prob=1.0
+    images,labels=input_data.read_files(test_dir)
+    num_test=len(labels)
+    max_step=int(num_test/BATCH_SIZE)
+    with tf.Graph().as_default():
+
+        test_batch,test_label=input_data.get_batch(images,labels,IMG_W,IMG_H,BATCH_SIZE,CAPACTIY)
+        test_logits=network_model.inference(test_batch,BATCH_SIZE,N_CLASS,keep_prob)
+        test_acc=network_model.evaluation(logits=test_logits,labels=test_label)
+
+        init=tf.global_variables_initializer()
+        saver=tf.train.Saver()
+        with tf.Session() as sess:
+            sess.run(init)
+            print 'reading checkpoint...'
+            ckpt = tf.train.get_checkpoint_state(log_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                global_stop = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                print 'load success'
+            else:
+                print 'No checkpoint'
+
+            print ':::the total number image is {0},step {1}:::'.format(num_test, max_step)
+
+            coord=tf.train.Coordinator()
+            threads=tf.train.start_queue_runners(sess=sess,coord=coord)
+            acc=0.0
+            try:
+                for step in range(max_step):
+                    if coord.should_stop():
+                        break
+                    tra_acc=sess.run(test_acc)
+                    print 'the step {0} accuracy is {1}'.format(step,tra_acc)
+                    acc+=tra_acc
+            except tf.errors.OutOfRangeError:
+                print 'done'
+            finally:
+                coord.request_stop()
+            result_accuracy=acc/max_step
+            print '{0} images , accuracy is {1}'.format(num_test,result_accuracy)
+            coord.join(threads)
+            sess.close()
+
+
+
+
+
 def evaluate_one_pic(stri):
+    keep_prob=1.0
     image=Image.open(stri)
     #image=image.convert('L')
     plt.imshow(image)
@@ -63,7 +125,7 @@ def evaluate_one_pic(stri):
         image=tf.cast(image_array,tf.float32)
         image=tf.image.rgb_to_grayscale(image)
         image=tf.reshape(image,[1,IMG_W,IMG_H,1])
-        logit=network_model.inference(image,batch_size,N_CLASS)
+        logit=network_model.inference(image,batch_size,N_CLASS,keep_prob)
 
         logit=tf.nn.softmax(logit)
         x=tf.placeholder(tf.float32,[IMG_W,IMG_H,3])
@@ -87,7 +149,8 @@ def evaluate_one_pic(stri):
 
 if __name__=='__main__':
     #run_training()
-    stri = '/home/saverio_sun/project/chinese_rec_data/test/03737/26695.png'
-    evaluate_one_pic(stri)
+    test()
+    # stri = '/home/saverio_sun/project/chinese_rec_data/train/00547/40187.png'
+    # evaluate_one_pic(stri)
 
 
